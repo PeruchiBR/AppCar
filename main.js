484c6f601d1d2b4728291b073c54c449b7f27dc3
PeruchiBR <32531351+PeruchiBR@users.noreply.github.com>
2018-12-19 09:12:30 -0200

    Add button remove

--- main.js
+++ main.js
@@ -1,73 +1,98 @@
-(function($) {
+(function (DOM) {
   'use strict';
+function app () {
+  return {
+    init: function init () {
+      this.bindEvents();
+      this.getCompanyInfo();
+    },
 
-  var app = (function() {
-    return {
-      init: function init() {
-        console.log('app init');
-        this.companyInfo();
-        this.initEvents();
-      },
+    getCompanyInfo: function getCompanyInfo () {
+      var ajax = new XMLHttpRequest();
+      ajax.open('GET', 'company.json', true);
+      //ajax.send();
+      ajax.addEventListener('readystatechange', this.handleReadyStateChange, false);
+    },
 
-      initEvents: function initEvents() {
-        $('[data-js="form-register"]').on('submit', this.handleSubmit);
-      },
-
-      handleSubmit: function handleSubmit(event) {
-        event.preventDefault();
-        console.log('submit');
-        var $tableCar = $('[data-js="table-car"]').get();
-        $tableCar.appendChild(app.createNewCar());
-      },
-
-      createNewCar: function createNewCar() {
-        var $fragment = document.createDocumentFragment();
-        var $tr = document.createElement('tr');
-        var $tdImage = document.createElement('td');
-        var $image = document.createElement('img');
-        var $tdBrand = document.createElement('td');
-        var $tdYear = document.createElement('td');
-        var $tdPlate = document.createElement('td');
-        var $tdColor = document.createElement('td');
-
-        $image.setAttribute('src', $('[data-js="image"]').get().value);
-        $tdImage.appendChild($image);
-
-        $tdBrand.textContent = $('[data-js="brand-model"]').get().value;
-        $tdYear.textContent = $('[data-js="year"]').get().value;
-        $tdPlate.textContent = $('[data-js="plate"]').get().value;
-        $tdColor.textContent = $('[data-js="color"]').get().value;
-
-        $tr.appendChild($tdImage);
-        $tr.appendChild($tdBrand);
-        $tr.appendChild($tdYear);
-        $tr.appendChild($tdPlate);
-        $tr.appendChild($tdColor);
-
-        return $fragment.appendChild($tr);
-      },
-
-      companyInfo: function companyInfo() {
-        var ajax = new XMLHttpRequest();
-        ajax.open('GET', '/company.json', true);
-        ajax.send();
-        ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
-      },
-
-      getCompanyInfo: function getCompanyInfo() {
-        if (!app.isReady.call(this)) return;
-        var data = JSON.parse(this.responseText);
-        var $companyName = $('[data-js="company-name"]');
-        var $companyPhone = $('[data-js="company-phone"]');
-        $companyName.textContent = data.name;
-        $companyPhone.textContent = data.phone;
-      },
-
-      isReady: function isReady() {
-        return this.readyState === 4 && this.status === 200;
+    handleReadyStateChange: function handleReadyStateChange () {
+      if (app().isReady.call(this)) {
+        app().populateCompanyInfo.call(this)
       }
-    };
-  })();
+    },
 
-  app.init();
-})(window.DOM);
+    populateCompanyInfo: function populateCompanyInfo () {
+      var companyInfo = JSON.parse(this.responseText);
+      new DOM('[data-js="company-name"]').get(0)[0].textContent = companyInfo.name;
+      new DOM('[data-js="company-phone"]').get(0)[0].textContent = companyInfo.phone;
+    },
+
+    isReady: function isReady () {
+      return this.readyState === 4 && this.status === 200;
+    },
+
+    bindEvents: function bindEvents () {
+      var $formRegister = new DOM('[data-js="form-register"]');
+
+      $formRegister.on('submit', this.handleSubmit);
+    },
+
+    handleSubmit: function handleSubmit (e) {
+      e.preventDefault();
+
+      var $tableCarsTbody = new DOM('[data-js="table-cars"] tbody').get(0)[0];
+
+      var $fragment = app().createNewCar();
+      $tableCarsTbody.appendChild($fragment);
+      app().bindEventRemoveCar($fragment);
+    },
+
+    createNewCar: function createNewCar () {
+      var $fragment = document.createDocumentFragment();
+      var $tr = document.createElement('tr');
+      var $tdImage = document.createElement('td');
+      var $image = document.createElement('img');
+      var $tdModel = document.createElement('td');
+      var $tdYear = document.createElement('td');
+      var $tdPlate = document.createElement('td');
+      var $tdColor = document.createElement('td');
+      var $tdAction = document.createElement('td');
+      var $button = document.createElement('button');
+
+      $image.src = new DOM('[data-js="image"]').get(0)[0].value;
+      $tdImage.appendChild($image);
+
+      $button.textContent = 'Remover';
+      $button.classList.add('btn');
+      $button.setAttribute('data-js', 'btn-remove');
+      $tdAction.appendChild($button);
+
+      $tdModel.textContent = new DOM('[data-js="model"]').get(0)[0].value;
+      $tdYear.textContent = new DOM('[data-js="year"]').get(0)[0].value;
+      $tdPlate.textContent = new DOM('[data-js="plate"]').get(0)[0].value;
+      $tdColor.textContent = new DOM('[data-js="color"]').get(0)[0].value;
+
+      $tr.appendChild($tdImage);
+      $tr.appendChild($tdModel);
+      $tr.appendChild($tdYear);
+      $tr.appendChild($tdPlate);
+      $tr.appendChild($tdColor);
+      $tr.appendChild($tdAction);
+
+      return $fragment.appendChild($tr);
+    },
+
+    bindEventRemoveCar: function bindEventRemoveCar ($fragment) {
+      $fragment.querySelector('[data-js="btn-remove"]').addEventListener('click', app().handleRemove);
+    },
+
+    handleRemove: function handleRemove (e) {
+      var $tableCarsTbody = new DOM('[data-js="table-cars"] tbody').get(0)[0];
+      var $tr = this.parentNode.parentNode;
+
+      $tableCarsTbody.removeChild($tr);
+    },
+  };
+}
+
+app().init();
+}) (DOM);
